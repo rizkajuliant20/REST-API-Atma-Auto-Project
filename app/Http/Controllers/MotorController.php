33b8b4719a87f537d36fa21589b84e1c3ede1c72
hendrikusray <hendrikusray@gmail.com>
2019-03-22 12:47:00 +0700

    MotorController dan API motor
    hai semua

--- app/Http/Controllers/MotorController.php
+++ app/Http/Controllers/MotorController.php
@@ -1,10 +1,141 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\motor;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Transformers\Motor_Transformer;
 
-class MotorController extends Controller
+class MotorController extends RestController
 {
-    //
+    protected $transformer = Motor_Transformer::class;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $motor = motor::all();
+        return response()->json($motor,200);
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'MERK_MOTOR' => 'required',
+            'TIPE_MOTOR' => 'required'
+        ]); 
+
+        try{
+            $motor = new motor;
+            $motor->MERK_MOTOR=$request->MERK_MOTOR;
+            $motor->TIPE_MOTOR=$request->TIPE_MOTOR;
+            $success=$motor->save();
+
+            if($success){
+                return response()->json('it is worked', 201);
+            }else{
+                return response()->json('failed to save the data!',500);
+            }
+
+        } catch (\Exception $e) {
+            return $this->sendIseResponse($e->getMessage());
+        }
+        
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+            $motor=motor::find($id);
+            $response = $this->generateItem($motor);
+            return $this->sendResponse($response);
+        } catch (ModelNotFoundException $e) {
+            return $this->sendNotFoundResponse('MOTOR TIDAK ADA');
+        } catch (\Exception $e) {
+            return $this->sendIseResponse($e->getMessage());
+        }
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try{
+            $motor=motor::find($id);
+            if(!is_null($request->MERK_MOTOR)){
+                $motor->MERK_MOTOR=$request->MERK_MOTOR;
+            }if(!is_null($request->TIPE_MOTOR))
+            {
+                $motor->TIPE_MOTOR=$request->TIPE_MOTOR;
+            }
+            $success=$motor->save();
+
+            if($success){
+                return response()->json('it is worked', 201);
+            }else{
+                return response()->json('failed to save the data!',500);
+            }
+        }catch(ModelNotFoundException $e) {
+            return $this->sendNotFoundResponse('motor TIDAK ADA');
+        }catch(\Exception $e) {
+            return $this->sendIseResponse($e->getMessage());
+        }
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $motor=motor::find($id);
+            $motor->delete();
+            return response()->json('Success',200);
+        } catch (ModelNotFoundException $e) {
+            return $this->sendNotFoundResponse('motor not found!');
+        } catch (\Exception $e) {
+            return $this->sendIseResponse($e->getMessage());
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('jasa_service','JasaServiceController');
+Route::resource('motor','MotorController');
